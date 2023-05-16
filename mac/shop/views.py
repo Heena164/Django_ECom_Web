e9906050f26379aa31a8ffac1f82ac95f4c8da9f
Heena164 <heenasr164@gmail.com>
2023-05-16 09:28:14 +0530

    Blog and Payment

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -8,7 +8,7 @@ from .PayTm import Checksum
 
 # Create your views here.
 from django.http import HttpResponse
-MERCHANT_KEY = 'kbzk1DSbJiV_03p5';
+MERCHANT_KEY = '4a0cheC3M27ZtZ_v';
 
 
 def index(request):
@@ -22,6 +22,30 @@ def index(request):
         allProds.append([prod, range(1, nSlides), nSlides])
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
+
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query= request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod=[item for item in prodtemp if searchMatch(query, item)]
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod)!= 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg":""}
+    if len(allProds)==0 or len(query)<4:                    #if user enter wrong input in search bar
+        params={'msg':"Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
 
 
 def about(request):
@@ -52,18 +76,14 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, order[0].items_json], default=str)
+                    response = json.dumps({"status":"success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
                 return HttpResponse(response)
             else:
-                return HttpResponse('{}')
+                return HttpResponse('{"status":"noitem"}')
         except Exception as e:
-            return HttpResponse('{}')
+            return HttpResponse('{"status":"error"}')
 
     return render(request, 'shop/tracker.html')
-
-
-def search(request):
-    return render(request, 'shop/search.html')
 
 
 def productView(request, myid):
@@ -95,7 +115,7 @@ def checkout(request):
         # Request paytm to transfer the amount to your account after payment by user
         param_dict = {
 
-                'MID': 'WorldP64425807474247',
+                'MID': 'IcJzZB60380119682987',
                 'ORDER_ID': str(order.order_id),
                 'TXN_AMOUNT': str(amount),
                 'CUST_ID': email,
